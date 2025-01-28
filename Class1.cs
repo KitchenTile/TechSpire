f5df083c1aee5bd069cc88418885bb247627b4f6
DomanskiFilip <152383917+DomanskiFilip@users.noreply.github.com>
2025-01-28 11:50:48 +0000

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// channel class
+public class Channel
+{
+    public string channelName { get; set; }
+    public string channelDescription { get; set; }
+    public string progrmaList { get; set; } // for now it is a string, but it should be a list of programs
+
+    // constructor
+    public Channel(string channelName, string channelDescription, string programList)
+    {
+        channelName = channelName;
+        channelDescription = channelDescription;
+        programList = programList;
+    }
+
+    public void DisplayChannel()
+    {
+        Console.WriteLine("Channel Name: " + channelName);
+        Console.WriteLine("Channel Description: " + channelDescription);
+        Console.WriteLine("Program List: " + progrmaList);
+    }
+
+    public void AddProgram(Program program)
+    {
+        progrmaList += program.programName + ", ";
+    }
+
+}   
