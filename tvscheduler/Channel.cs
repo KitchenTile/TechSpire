9487e8bf3de7f7d0cc52503ff54bf0fe7c0390bb
DomanskiFilip <filip_domanski@outlook.com>
2025-01-29 15:41:21 +0000

    deleting constuctor in Channel.cs

--- tvscheduler/Channel.cs
+++ tvscheduler/Channel.cs
@@ -6,15 +6,7 @@ public class Channel
     public required string Name { get; set; }
     public required string ChannelDescription  { get; set; }
     public required List<Show> ShowList { get; set; }
-
-    // constructor
-    // public Channel(int channelId, string name, List<Show> showlist)
-    // {
-    //     ChannelId = channelId;
-    //     Name = name;
-    //     ShowList = showlist;
-    // }
-
+    
     public void DisplayChannel()
     {
         Console.WriteLine("Channel Name: " + Name);
