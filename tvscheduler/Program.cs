2371df90ea2616914becd6f5e38c037fe0b59441
KitchenTile <azuldebenedetti@gmail.com>
2025-01-29 16:30:37 +0000

    Test new ruleset
    Changes on channel to properly display contents of Showlist, mainly to check for ruleset working properly.

--- tvscheduler/Program.cs
+++ tvscheduler/Program.cs
@@ -20,6 +20,9 @@ app.UseHttpsRedirection();
 var mockShow = new Show { Id = 1, Name = "Mock Show", StartTime = 1900, EndTime = 2000, ChannelId = 5 };
 var mockChannel = new Channel { ChannelId = 01, Name = "Mock Channel", ChannelDescription = "mock desc", ShowList = [mockShow] };
 
+
+mockChannel.DisplayChannel();
+
 // test endpoint
 app.MapGet("/", () =>
 {
