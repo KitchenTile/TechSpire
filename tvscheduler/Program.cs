585c0dbf39c85bbb06e34ce40cdd177ec687778f
CasualStrategy <filip_domanski@outlook.com>
2025-01-30 12:25:46 +0000

    connected shows and channels through list

--- tvscheduler/Program.cs
+++ tvscheduler/Program.cs
@@ -16,12 +16,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+var mockChannel = new Channel { ChannelId = 1, Name = "Mock Channel", ChannelDescription = "mock desc", ShowList = new List<Show>() };
+var mockChannel2 = new Channel { ChannelId = 2, Name = "Mock Channel 2", ChannelDescription = "mock desc", ShowList = new List<Show>() };
 
-var mockShow = new Show { Id = 1, Name = "Mock Show", StartTime = 1900, EndTime = 2000, ChannelId = [1, 2]};
-var mockShow2 = new Show { Id = 2, Name = "Mock Show2", StartTime = 1600, EndTime = 1900, ChannelId = [1]};
-var mockShow3 = new Show { Id = 3, Name = "Mock Show3", StartTime = 2000, EndTime = 2100, ChannelId = [1, 2]};
-var mockChannel = new Channel { ChannelId = 1, Name = "Mock Channel", ChannelDescription = "mock desc", ShowList = [mockShow, mockShow2, mockShow3] };
-var mockChannel2 = new Channel { ChannelId = 2, Name = "Mock Channel 2", ChannelDescription = "mock desc", ShowList = [mockShow, mockShow3] };
+var mockShow = new Show { Id = 1, Name = "Mock Show", StartTime = 1900, EndTime = 2000, ChannelId = new List<Channel> { mockChannel, mockChannel2 } };
+var mockShow2 = new Show { Id = 2, Name = "Mock Show2", StartTime = 1600, EndTime = 1900, ChannelId = new List<Channel> { mockChannel } };
+var mockShow3 = new Show { Id = 3, Name = "Mock Show3", StartTime = 2000, EndTime = 2100, ChannelId = new List<Channel> { mockChannel, mockChannel2 } };
+
+mockChannel.ShowList = new List<Show> { mockShow, mockShow2, mockShow3 };
+mockChannel2.ShowList = new List<Show> { mockShow, mockShow3 };
 
 mockChannel.DisplayChannel();
 Console.WriteLine("------------");
