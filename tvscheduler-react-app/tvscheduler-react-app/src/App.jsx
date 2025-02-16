3746b7278626a33e165203a60e707ddad6de9218
KitchenTile <azuldebenedetti@gmail.com>
2025-02-16 22:02:46 +0000

    Implemented router and worked on Login page

--- tvscheduler-react-app/tvscheduler-react-app/src/App.jsx
+++ tvscheduler-react-app/tvscheduler-react-app/src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import MainSchedulePage from "./pages/mainSchedulePage";
 import LoginRegisterPage from "./pages/loginPage.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <MainSchedulePage />
-      {/* <LoginRegisterPage /> */}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/main" element={<MainSchedulePage />} />
+        <Route path="/login" element={<LoginRegisterPage />} />
+      </Routes>
+    </Router>
   );
 }
 
