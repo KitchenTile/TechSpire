49605c3391324c6d9ab82d3972fc25403e92dd5f
Blue <azuldebenedetti@gmail.com>
2025-03-24 15:02:26 +0000

    Recommendations

--- tvscheduler-react-app/tvscheduler-react-app/src/pages/mainSchedulePage.jsx
+++ tvscheduler-react-app/tvscheduler-react-app/src/pages/mainSchedulePage.jsx
@@ -10,12 +10,15 @@ import AddRemoveShowsContextProvider from "../contexts/AddRemoveShowsContextProv
 import useThrottle from "../hooks/useThrottle";
 import Modal from "../components/misc/Modal";
 import { useNavigate } from "react-router-dom";
+import useMergeAndFilter from "../hooks/useMergeAndFilter";
+import ShowCard from "../components/showScheduler/ShowCard";
 
 const MainSchedulePage = () => {
   const channels = useContext(ChannelsContext);
   const [isVisible, setIsVisible] = useState(false);
   const [openModal, setOpenModal] = useState(true);
   const navigate = useNavigate();
+  const mergedAndFilteredShows = useMergeAndFilter("All");
 
   //effect to go back to the login page if the JWT expires
   useEffect(() => {
@@ -52,6 +55,20 @@ const MainSchedulePage = () => {
     setOpenModal(false);
   };
 
+  const recommendations = () => {
+    let userTags = [];
+
+    userTags = channels?.favTags.map((tag) => {
+      return tag.tagName;
+    });
+
+    return mergedAndFilteredShows.filter((show) =>
+      userTags.includes(show.tagName)
+    );
+  };
+
+  const recommnededShows = recommendations();
+
   return (
     <div className="page-container">
       {channels ? (
@@ -66,14 +83,17 @@ const MainSchedulePage = () => {
             <SectionCarouselComponent />
             {/* my shows display */}
             <MyShowsComponent />
-            <h1 className="title h1">All Shows</h1>
+            <h1 className="title h1">Our Picks For You</h1>
             <div className="grid-container">
               {/* get the first x elements of the guide data array -- 129 is too long man */}
-              {channels.channels.map((channel) => (
+              {/* {channels.channels.map((channel) => (
                 <ChannelShowComponent
                   key={channel.channelId}
                   channel={channel}
                 />
+              ))} */}
+              {recommnededShows.map((show) => (
+                <ShowCard show={show} />
               ))}
             </div>
           </>
