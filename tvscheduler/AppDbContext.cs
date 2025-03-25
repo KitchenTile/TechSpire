8894b9d4791b182103fe576c0c125ad0d6558534
Dominik Wujek <dom.wujek@gmail.com>
2025-03-25 16:22:28 +0000

    Fix foreign keys in recommendations models

--- tvscheduler/AppDbContext.cs
+++ tvscheduler/AppDbContext.cs
@@ -16,7 +16,7 @@ public class AppDbContext : IdentityDbContext<User>
     public DbSet<FavouriteTag> FavouriteTags { get; set; }
     public DbSet<FavouriteShow> FavouriteShows { get; set; }
     
-    public DbSet<RecommendationsGlobal> GlobalRecommendations { get; set; }
+    public DbSet<RecommendationGlobal> GlobalRecommendations { get; set; }
     public DbSet<RecommendationForUser> IndividualRecommendations { get; set; }
     
     
